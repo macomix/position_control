57b8c901d60617d418a0b194117046ff6a47e297
Thies Lennart Alff <lennart.alff@gmail.com>
2023-12-02 15:42:55 +0000

    added very basic example of a yaw controller

---
+++ nodes/yaw_controller.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+import math
+
+import rclpy
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from hippo_msgs.msg import ActuatorSetpoint, Float64Stamped
+from rclpy.node import Node
+from rclpy.qos import QoSHistoryPolicy, QoSProfile, QoSReliabilityPolicy
+from tf_transformations import euler_from_quaternion
+
+
+class YawController(Node):
+
+    def __init__(self):
+        super().__init__(node_name='yaw_controller')
+        self.setpoint = 0.0
+        qos = QoSProfile(reliability=QoSReliabilityPolicy.BEST_EFFORT,
+                         history=QoSHistoryPolicy.KEEP_LAST,
+                         depth=1)
+        self.vision_pose_sub = self.create_subscription(
+            msg_type=PoseWithCovarianceStamped,
+            topic='vision_pose_cov',
+            callback=self.on_vision_pose,
+            qos_profile=qos)
+
+        self.setpoint_sub = self.create_subscription(Float64Stamped,
+                                                     topic='~/setpoint',
+                                                     callback=self.on_setpoint,
+                                                     qos_profile=qos)
+        self.torque_pub = self.create_publisher(msg_type=ActuatorSetpoint,
+                                                topic='torque_setpoint',
+                                                qos_profile=1)
+
+    def wrap_pi(self, value: float):
+        """Normalize the angle to the range [-pi; pi]."""
+        if (-math.pi < value) and (value < math.pi):
+            return value
+        range = 2 * math.pi
+        num_wraps = math.floor((value + math.pi) / range)
+        return value - range * num_wraps
+
+    def on_setpoint(self, msg: Float64Stamped):
+        self.setpoint = self.wrap_pi(msg.data)
+
+    def on_vision_pose(self, msg: PoseWithCovarianceStamped):
+        # get the vehicle orientation expressed as quaternion
+        q = msg.pose.pose.orientation
+        # convert the quaternion to euler angles
+        (roll, pitch, yaw) = euler_from_quaternion([q.x, q.y, q.z, q.w])
+        #yaw = self.wrap_pi(yaw)
+
+        control_output = self.compute_control_output(yaw)
+        timestamp = rclpy.time.Time.from_msg(msg.header.stamp)
+        self.publish_control_output(control_output, timestamp)
+
+    def compute_control_output(self, yaw):
+        # very important normalize the angle error!
+        error = self.wrap_pi(self.setpoint - yaw)
+        return 0.1 * error
+
+    def publish_control_output(self, control_output: float,
+                               timestamp: rclpy.time.Time):
+        msg = ActuatorSetpoint()
+        msg.header.stamp = timestamp.to_msg()
+        msg.ignore_x = True
+        msg.ignore_y = True
+        msg.ignore_z = False  # yaw is the rotation around the vehicle's z axis
+
+        msg.z = control_output
+        self.torque_pub.publish(msg)
+
+
+def main():
+    rclpy.init()
+    node = YawController()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()
