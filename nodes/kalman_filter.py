d665f0df587262f73ccc925f520a47ff09badfb5
NBauschmann <nathalie_bauschmann@hotmail.de>
2023-12-02 17:33:31 +0100

    empty template

--- nodes/kalman_filter.py
+++ nodes/kalman_filter.py
@@ -26,7 +26,6 @@ class PositionKalmanFilter(Node):
         self.num_states = 3
 
         # initial state
-        # TODO pick a reasonable initial state
         self.x0 = np.zeros((self.num_states, 1))
 
         # state, this will be updated in Kalman filter algorithm
@@ -41,8 +40,8 @@ class PositionKalmanFilter(Node):
         # state covariance, this will be updated in Kalman filter algorithm
         self.P = self.P0
 
-        # process noise covariance - how much noise do we add at each process
-        # update step?
+        # process noise covariance - how much noise do we add at each
+        # prediction step?
         # TODO tuning knob
         # dimension: num states x num states
         # matrix needs to be positive definite and symmetric
@@ -59,8 +58,8 @@ class PositionKalmanFilter(Node):
         # TODO enter tag poses here
         # TODO in the experiment, the tags will not be in these exact positions
         # however, the relative positions between tags will be the same
-        self.tag_poses = np.array([[0.7, 3.8, -0.5], [1.3, 3.8, -0.5],
-                                   [0.7, 3.8, -0.9], [1.3, 3.8, -0.9]])
+        self.tag_poses = np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0],
+                                   [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]])
 
         self.position_pub = self.create_publisher(msg_type=PoseStamped,
                                                   topic='position_estimate',
@@ -72,9 +71,9 @@ class PositionKalmanFilter(Node):
             callback=self.on_ranges,
             qos_profile=qos)
 
-        # do process update with 50 Hz
+        # do prediction step with 50 Hz
         self.process_update_timer = self.create_timer(
-            1.0 / 50, self.on_process_update_timer)
+            1.0 / 50, self.on_prediction_step_timer)
 
     def init_params(self):
         self.declare_parameters(namespace='',
@@ -112,84 +111,39 @@ class PositionKalmanFilter(Node):
         if not num_measurements:
             return
 
-        # initialize some matrices to save the detected tags
-        detected_tag_poses = np.zeros((num_measurements, 3))
-        distance_measurements = np.zeros((num_measurements, 1))
-
         measurement: RangeMeasurement
         for index, measurement in enumerate(ranges_msg.measurements):
             # TODO
-            # What tag id was this?
-            tag_id = int(measurement.id)
-
-            # add this tag's information to list of detected tags
-            detected_tag_poses[index, :] = self.tag_poses[tag_id, :]
-            distance_measurements[index, 0] = measurement.range
+            pass
 
         # before the measurement update, let's do a process update
         now = self.get_clock().now()
         dt = (now - self.time_last_prediction).nanoseconds * 1e-9
-        self.process_update(dt)
+        self.prediction(dt)
         self.time_last_prediction = now
 
-        self.measurement_update(detected_tag_poses, distance_measurements)
+        # TODO
+        # self.measurement_update(...)
 
-    def on_process_update_timer(self):
-        # We will do a process update aka prediction with a constant rate
+    def on_prediction_step_timer(self):
+        # We will do a prediction step with a constant rate
         now = self.get_clock().now()
         dt = (now - self.time_last_prediction).nanoseconds * 1e-9
 
-        self.process_update(dt)
+        self.prediction(dt)
         self.time_last_prediction = now
 
         # publish the estimated pose with constant rate
         self.publish_pose_msg(state=np.copy(self.state), now=now)
 
-    def measurement_update(self, tag_positions, distance_measurements):
-        # TODO
+    def measurement_update(self):
         vehicle_position = np.copy(self.state[0:3, 0])
-
-        num_measurements = np.shape(distance_measurements)[0]
-        R = (self.range_noise_stddev**2) * np.eye(num_measurements)
-
-        z_est = np.zeros((num_measurements, 1))
-        H = np.zeros((num_measurements, self.num_states))
-
-        for index, tag_position in enumerate(tag_positions):
-            distance_vector = tag_position - vehicle_position
-            # this is the distance that we would have expected based on the
-            # current estimate of the vehicle position.
-            dist_est = np.linalg.norm(distance_vector)
-            z_est[index, 0] = dist_est
-
-            # dh / dx = 1/2 * (dist ** 2)^(-1/2) * (2 * (x1 - t1) * 1)
-            H[index, 0:3] = [(vehicle_position[0] - tag_position[0] / dist_est),
-                             (vehicle_position[1] - tag_position[1] / dist_est),
-                             (vehicle_position[2] - tag_position[2] / dist_est)]
-
-        y = distance_measurements - z_est
-
-        # tmp = np.matmul(np.matmul(H, self.P), H.transpose()) + R
-        # K = np.matmul(np.matmul(self.P, H.transpose()), np.linalg.inv(tmp))
-        # @nbauschmann why not the notation below?
-        tmp = H @ self.P @ H.transpose() + R
-        K = self.P @ H.transpose() @ np.linalg.inv(tmp)
-
-        # update state
-        self.state = self.state + K @ y
-        # update covariance
-        P_tmp = np.eye(self.num_states) - K @ H
-        self.P = P_tmp @ self.P
-
-    def process_update(self, dt: float):
         # TODO
-        A = np.eye(3)
+        pass
 
-        # self.state = np.matmul(np.copy(A), self.state)
-        self.state = A @ self.state
-        # self.P = np.matmul(np.matmul(np.copy(A), np.copy(self.P)),
-        #                   (np.transpose(np.copy(A)))) + self.Q
-        self.P = A @ self.P @ A.transpose() + self.Q
+    def prediction(self, dt: float):
+        # TODO
+        pass
 
     def publish_pose_msg(self, state: np.ndarray, now: rclpy.time.Time) -> None:
         msg = PoseStamped()
