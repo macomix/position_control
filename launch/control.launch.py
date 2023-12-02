dd1f804c185c99ed547c8458a7274024f2296071
Thies Lennart Alff <lennart.alff@gmail.com>
2023-12-02 16:45:19 +0000

    add control launch file

---
+++ launch/control.launch.py
@@ -0,0 +1,22 @@
+from launch_ros.actions import Node, PushRosNamespace
+
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    GroupAction,
+)
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description() -> LaunchDescription:
+    launch_description = LaunchDescription()
+    arg = DeclareLaunchArgument('vehicle_name')
+    launch_description.add_action(arg)
+
+    group = GroupAction([
+        PushRosNamespace(LaunchConfiguration('vehicle_name')),
+        Node(executable='yaw_controller.py', package='position_control'),
+        # add your other controllers here
+    ])
+    launch_description.add_action(group)
+    return launch_description
