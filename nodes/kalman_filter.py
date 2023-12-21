added config and fixed quaternions

nodes/kalman_filter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Extended Kalman filter
+Extended Kalman filter (EKF)
 """
 
 import numpy as np
@@ -13,6 +13,10 @@ from rclpy.qos import QoSHistoryPolicy, QoSProfile, QoSReliabilityPolicy
 
 from tf_transformations import euler_from_quaternion
 
+class SimpleRangeMeasurement():
+    def __init__(self,range,tag_id) -> None:
+        self.range = range
+        self.id = tag_id
 
 class PositionKalmanFilter(Node):
 
@@ -48,7 +52,7 @@ class PositionKalmanFilter(Node):
         # TODO initial state covariance is tuning knob
         # dimension: num states x num states
         # matrix needs to be positive definite and symmetric
-        self.P0 = 0.1 * np.eye(self.num_states)
+        self.P0 = 0.01 * np.eye(self.num_states)
 
         # state covariance, this will be updated in Kalman filter algorithm
         self.P = self.P0
@@ -62,14 +66,14 @@ class PositionKalmanFilter(Node):
 
         # TODO enter tag poses here
         # however, the relative positions between tags will be the same
-        # self.tag_poses = np.array([[0.7, 3.8, -0.5], [1.3, 3.8, -0.5], 
-        #                            [0.7, 3.8, -0.9], [1.3, 3.8, -0.9]])
+        self.tag_poses = np.array([[0.7, 3.8, -0.5], [1.3, 3.8, -0.5], 
+                                   [0.7, 3.8, -0.9], [1.3, 3.8, -0.9]])
         
-        self.offsetXYZ = np.array([0.634, 3.8, -0.498])
-        self.tag_poses = np.array([[self.offsetXYZ[0], self.offsetXYZ[1], self.offsetXYZ[2]], 
-                                   [self.offsetXYZ[0] + 0.635, self.offsetXYZ[1], self.offsetXYZ[2]], 
-                                   [self.offsetXYZ[0], self.offsetXYZ[1], -0.89], 
-                                   [self.offsetXYZ[0] + 0.635, self.offsetXYZ[1], -0.89]])
+        # self.offsetXYZ = np.array([0.634, 3.8, -0.498])
+        # self.tag_poses = np.array([[self.offsetXYZ[0], self.offsetXYZ[1], self.offsetXYZ[2]], 
+        #                            [self.offsetXYZ[0] + 0.635, self.offsetXYZ[1], self.offsetXYZ[2]], 
+        #                            [self.offsetXYZ[0], self.offsetXYZ[1], -0.89], 
+        #                            [self.offsetXYZ[0] + 0.635, self.offsetXYZ[1], -0.89]])
 
         # publisher
         self.position_pub = self.create_publisher(msg_type=PoseStamped,
@@ -119,7 +123,6 @@ class PositionKalmanFilter(Node):
                 self.range_noise_stddev = param.get_parameter_value().double_value
             elif param.name == 'process_noise_position_stddev':
                 self.process_noise_position_stddev = param.get_parameter_value().double_value
-                self.Q = (self.process_noise_position_stddev ** 2) * np.eye(self.num_states)
             else:
                 continue
         return SetParametersResult(successful=True, reason='Parameter set')
@@ -132,28 +135,20 @@ class PositionKalmanFilter(Node):
         if not self.num_measurements:
             return
 
-        # TODO: probably collect measurements and calculate position
-        measurements: list[tuple[float, int]] = []
+        # only save necessary information for ease of use
+        measurements: list[SimpleRangeMeasurement] = []
 
         measurement: RangeMeasurement
         for index, measurement in enumerate(ranges_msg.measurements):
-            tag_id = measurement.id
-            measured_distance = measurement.range
-            # self.get_logger().info(
-            #     f'The {index}. element contains the measurement of tag {tag_id} with the '
-            #     f'distance of {measured_distance}m')
-            # TODO
-            measurements.append((measured_distance, tag_id))
-            #measurements[index] = [measured_distance, tag_id]
-
-        #self.get_logger().info(f'Measurements {measurements}')
+            new_measurement = SimpleRangeMeasurement(range=measurement.range, tag_id=measurement.id)
+            measurements.append(new_measurement)
 
         # before the measurement update, let's do a process update
         now = self.get_clock().now()
         dt = (now - self.time_last_prediction).nanoseconds * 1e-9
 
-        #self.get_logger().info(f'dt: {dt}, gain: {self.process_noise_position_stddev**2}')
-        self.Q = self.get_matrix_Q(dt) # update process noise
+        # and update the process noise
+        self.Q = self.get_matrix_Q(dt) 
 
         self.prediction(dt)
         self.time_last_prediction = now
@@ -187,21 +182,24 @@ class PositionKalmanFilter(Node):
         # publish the estimated pose with constant rate
         self.publish_pose_msg(state=np.copy(self.state), now=now)
 
-    def measurement_update(self, measurements: list[tuple[float, int]]):
+    def measurement_update(self, measurements: list[SimpleRangeMeasurement]):
+        """
+        Update step of the Kalman filter algorithm.
+
+        Args:
+            measurements (list[SimpleRangeMeasurement]): range measurement list
+        """
         vehicle_position = np.copy(self.state[0:3, 0])
         
-        # diff between estimated measurement based on predicted position and actual measurements
         y = self.get_innovation_y(vehicle_position, measurements)
 
-        # jacobian of observation TODO: probably change the whole calculation of H
         H = self.get_jacobian_H(vehicle_position, measurements)
 
         # covariance of the observation noise (dim: [m, m])
         R = (self.range_noise_stddev ** 2) * np.eye(self.num_measurements)
 
-        # compute Kalman gain
         S = H @ self.P @ H.transpose() + R   # innovation covariance
-        K = self.P @ H.transpose() @ np.linalg.inv(S)
+        K = self.P @ H.transpose() @ np.linalg.inv(S)   # Kalman gain
 
         # update state
         state_next = self.state + K @ y
@@ -209,18 +207,16 @@ class PositionKalmanFilter(Node):
         # update covariance
         P_next = (np.eye(self.num_states) - (K @ H)) @ self.P
 
-        #self.get_logger().info(f'y: {y}')
-        #self.get_logger().info(f'Next state: {state_next}' f'Current state: {self.state}')
-
         self.state = state_next
         self.P = P_next
 
-    def get_innovation_y(self, vehicle_position_est, measurements) -> np.ndarray:
-        """Calculates the difference between the prediction and the observation.
+    def get_innovation_y(self, vehicle_position_est: np.ndarray, measurements: list[SimpleRangeMeasurement]) -> np.ndarray:
+        """
+        Calculates the difference between the prediction and the observation.
 
         Args:
-            vehicle_position_est (_type_): x,y,z position of the robot
-            measurements (_type_): all of the measurements in an array
+            vehicle_position_est (np.ndarray): x,y,z position of the robot
+            measurements (list[SimpleRangeMeasurement]): all of the measurements in an array
 
         Returns:
             np.ndarray: innovation y (dim: [m, 1])
@@ -229,17 +225,21 @@ class PositionKalmanFilter(Node):
 
         for index, measurement in enumerate(measurements):
             # y = z-z_estimated
-            tag_id = measurement[1]
-            y[index] = measurement[0] - np.linalg.norm(self.tag_poses[tag_id] - vehicle_position_est)
+            tag_id = measurement.id
+            y[index] = measurement.range - np.linalg.norm(self.tag_poses[tag_id] - vehicle_position_est).astype(float)
 
         return y.reshape(-1, 1)
     
-    def get_jacobian_H(self, vehicle_position, measurements) -> np.ndarray:
-        """Observation model, which estimates measurements based on the estimated state.
+    def get_jacobian_H(self, vehicle_position: np.ndarray, measurements: list[SimpleRangeMeasurement]) -> np.ndarray:
+        """
+        Observation model, which estimates measurements based on the estimated state.
+        For EKF H is basically the Jacobian of the measurements:
+          z = H*x + v
+          z = distance
 
         Args:
-            vehicle_position (_type_): x,y,z position of the robot
-            measurements (_type_): all of the measurements in an array
+            vehicle_position (np.ndarray): x,y,z position of the robot
+            measurements (list[SimpleRangeMeasurement]): all of the measurements in an array
 
         Returns:
             np.ndarray: observation matrix (dim: [m,n])
@@ -248,7 +248,7 @@ class PositionKalmanFilter(Node):
         H = np.zeros((self.num_measurements, self.num_states))
         
         for index, measurement in enumerate(measurements):
-            tag_id = measurement[1]
+            tag_id = measurement.id
             distance = np.linalg.norm(self.tag_poses[tag_id] - vehicle_position)
             part_der_x = (vehicle_position[0]-self.tag_poses[tag_id, 0])/distance
             part_der_y = (vehicle_position[1]-self.tag_poses[tag_id, 1])/distance
@@ -257,8 +257,9 @@ class PositionKalmanFilter(Node):
         
         return H
     
-    def get_matrix_A(self, dt: float) -> np.ndarray:
-        """This function creates the jacobian matrix 
+    def get_jacobian_A(self, dt: float) -> np.ndarray:
+        """
+        This function creates the jacobian matrix 
         for the state-transition model (matrix A or F).
 
         Args:
@@ -275,7 +276,8 @@ class PositionKalmanFilter(Node):
         return np.concatenate((A,B), axis=0)
 
     def get_matrix_Q(self, dt: float) -> np.ndarray:
-        """process noise covariance - How much noise do we add at each prediction step? 
+        """
+        process noise covariance - How much noise do we add at each prediction step? 
         -> The higher dt the more time between measurements
         which results in a higher uncertainty.
         source 1: Wikipedia
@@ -304,14 +306,15 @@ class PositionKalmanFilter(Node):
         return (self.process_noise_position_stddev**2) * Q
 
     def prediction(self, dt: float):
-        """This function is the first part of the Kalman-Filter.
+        """
+        This function is the first part of the Kalman-Filter.
         It makes a prediction on the current position of the robot.
 
         Args:
             dt (float): delta time
         """
 
-        matrix_A = self.get_matrix_A(dt)
+        matrix_A = self.get_jacobian_A(dt)
         self.state = matrix_A @ self.state # estimated state
         self.P = matrix_A @ self.P @ matrix_A.transpose() + self.Q # estimated covariance matrix
 
@@ -338,6 +341,8 @@ class PositionKalmanFilter(Node):
         msg.vector.z = state[5, 0]
 
         self.velocity_pub.publish(msg)
+
+
 
 def main():
     rclpy.init()

nodes/pose_setpoint.py
@@ -19,7 +19,7 @@ class PoseSetpointNode(Node):
         self.start_time = self.get_clock().now()
 
         # change these parameters to adjust the setpoints
-        self.duration = 20.0  # in seconds
+        self.duration = 30.0  # in seconds
 
         self.position_setpoint_pub = self.create_publisher(msg_type=Vector3Stamped,
                                                         topic='position_setpoint',
@@ -41,7 +41,7 @@ class PoseSetpointNode(Node):
         now = self.get_clock().now()
         time = self.start_time - now
 
-        position = np.array([1.0, 2.0, -0.3])
+        position = np.array([1.0, 2.5, -0.3])
         velocity = np.zeros(3)
 
         function = 0
@@ -50,18 +50,13 @@ class PoseSetpointNode(Node):
                 # square sine
                 i = time.nanoseconds * 1e-9 % (self.duration * 2)
                 if i > (self.duration):
-                    position =  np.array([0.8, 2.0, -0.5])
+                    position =  np.array([0.8, 2.5, -0.3])
                 else:
-                    position =  np.array([1.5, 2.5, -0.8])
+                    position =  np.array([1.2, 1.5, -0.6])
 
                 # set the velocity for square wave zero
                 velocity = np.zeros(3)
             case 1:
-                # something
-                #posA = np.array([0.8, 2.0, -0.5])
-                #posB = np.array([1.5, 2.5, -0.5])
-                pass
-            case 2:
                 # circle on xy-plane
                 center = np.array([1.0, 2.0, -0.5])
                 radius = 0.5

nodes/position_controller.py
@@ -5,10 +5,9 @@ based on a given input position.
 """
 import numpy as np
 
-from sympy import euler
-
 import rclpy
 from hippo_msgs.msg import ActuatorSetpoint
+from rcl_interfaces.msg import SetParametersResult
 from rclpy.node import Node
 from rclpy.qos import QoSHistoryPolicy, QoSProfile, QoSReliabilityPolicy
 from geometry_msgs.msg import PoseStamped, Vector3Stamped, Vector3, PoseWithCovarianceStamped
@@ -24,9 +23,18 @@ class PositionControlNode(Node):
         qos = QoSProfile(reliability=QoSReliabilityPolicy.BEST_EFFORT,
                          history=QoSHistoryPolicy.KEEP_LAST,
                          depth=1)
+        
+        # gains for each direction [k_p, k_i, k_d]
+        self.gains_x = np.zeros(3)
+        self.gains_y = np.zeros(3)
+        self.gains_z = np.zeros(3)
+        self.init_params()
+        self.add_on_set_parameters_callback(self.on_params_changed)
+
+        self.get_logger().info(f'{self.gains_x}')
 
         self.current_setpoint = np.zeros(3)
-        self.rotation_matrix = np.eye(3)
+        self.quaternion = np.zeros(4) # w, x, y, z
 
         self.velocity = np.zeros(3)
         self.setpoint_velocity = np.zeros(3)
@@ -71,6 +79,38 @@ class PositionControlNode(Node):
                                                     callback=self.on_vision_pose,
                                                     qos_profile=qos)
 
+    def init_params(self):
+        # load params from config
+        self.declare_parameters(namespace='',
+                                parameters=[
+                                    ("gains_x", rclpy.Parameter.Type.DOUBLE_ARRAY),
+                                    ("gains_y", rclpy.Parameter.Type.DOUBLE_ARRAY),
+                                    ("gains_z", rclpy.Parameter.Type.DOUBLE_ARRAY)
+                                             ])
+        
+        param = self.get_parameter('gains_x')
+        self.gains_x = param.get_parameter_value().double_array_value
+
+        param = self.get_parameter('gains_y')
+        self.gains_y = param.get_parameter_value().double_array_value
+
+        param = self.get_parameter('gains_z')
+        self.gains_z = param.get_parameter_value().double_array_value
+
+    def on_params_changed(self, params):
+        param: rclpy.Parameter
+        for param in params:
+            self.get_logger().info(f'Try to set [{param.name}] = {param.value}')
+            if param.name == 'gains_x':
+                self.gains_x = param.get_parameter_value().double_array_value
+            elif param.name == 'gains_y':
+                self.gains_y = param.get_parameter_value().double_array_value
+            elif param.name == 'gains_z':
+                self.gains_z = param.get_parameter_value().double_array_value
+            else:
+                continue
+        return SetParametersResult(successful=True, reason='Parameter set')
+    
     def on_setpoint(self, setpoint_msg: Vector3Stamped):
         # on setpoint message received save data
         self.current_setpoint = np.array([setpoint_msg.vector.x,setpoint_msg.vector.y,setpoint_msg.vector.z])
@@ -95,6 +135,9 @@ class PositionControlNode(Node):
         # option 2:
         timestamp = rclpy.time.Time.from_msg(position_msg.header.stamp) # type: ignore
 
+        if np.array_equal(self.quaternion, np.zeros(4)):
+            self.get_logger().warn(f'nothing yet received from vision_pose_cov')
+        
         thrust = self.compute_control_output(current_position, timestamp)
 
         self.publish_thrust(thrust=thrust, timestamp=timestamp)
@@ -106,11 +149,10 @@ class PositionControlNode(Node):
     def on_vision_pose(self, msg: PoseWithCovarianceStamped):
         # get the vehicle orientation expressed as quaternion
         q = msg.pose.pose.orientation
-        # convert quaternion to euler angles
-        (roll, pitch, yaw) = euler_from_quaternion([q.x, q.y, q.z, q.w])
+        # convert quaternion to euler angles -> dont
+        #(roll, pitch, yaw) = euler_from_quaternion([q.x, q.y, q.z, q.w])
 
-        #self.rotation_matrix = euler_rotation_matrix(0, 0, yaw)
-        self.rotation_matrix = quaternion_rotation_matrix(q.w, q.x, q.y, q.z)
+        self.quaternion = np.array([q.w, q.x, q.y, q.z])
 
     def publish_thrust(self, thrust: np.ndarray, 
                        timestamp: rclpy.time.Time) -> None: # type: ignore
@@ -131,12 +173,8 @@ class PositionControlNode(Node):
 
     def compute_control_output(self, current_position: np.ndarray, 
                                timestamp: rclpy.time.Time) -> np.ndarray: # type: ignore
-        # gains for each direction [k_p, k_i, k_d]
-        # TODO: z should depend on direction because of bouyancy
-        gain_x = np.array([0.0, 0.0, 0.0]) # ([1.0, 0.05, 3.0])
-        gain_y = np.array([0.0, 0.0, 0.0]) # ([1.0, 0.05, 3.0])
-        gain_z = np.array([0.0, 0.0, 0.0]) # ([1.0, 0.05, 3.0])
-        gain = np.array([gain_x, gain_y, gain_z])
+        
+        gain = np.array([self.gains_x, self.gains_y, self.gains_z])
 
         # safe area for the robot to operate [min, max]
         safe_space_x = np.array([0.1, 1.9])
@@ -176,11 +214,11 @@ class PositionControlNode(Node):
                 thrust[i] = 0.0
 
         # convert to local space of the robot
-        thrust = np.matmul(self.rotation_matrix.transpose(), thrust.reshape((-1,1)))
+        thrust = vector3_rotate(vector3=thrust, quaternion=self.quaternion)
         
         # publish some information for debugging and documentation
         self.publish_pid_info(gain, error, self.error_integral, derivative_error, timestamp)
-
+        
         self.last_time = self.get_clock().now().nanoseconds * 10e-9
         return thrust.reshape(-1)
 
@@ -201,7 +239,6 @@ class PositionControlNode(Node):
         
         self.pid_debug_pub.publish(msg)
         
-# TODO: put utility functions into separate file
 def clamp(number, smallest, largest):
     return max(smallest, min(number, largest))
 
@@ -217,48 +254,19 @@ def numpy_to_vector3(array: np.ndarray) -> Vector3:
     
     return rosVector
 
-def quaternion_rotation_matrix(w, x, y, z):
-     
-    # First row of the rotation matrix
-    r00 = 1-2*y*y-2*z*z
-    r01 = 2*x*y-2*z*w
-    r02 = 2*x*z+2*y*w
-     
-    # Second row of the rotation matrix
-    r10 = 2*x*y+2*z*w
-    r11 = 1-2*x*x-2*z*z
-    r12 = 2*y*z-2*x*w
-     
-    # Third row of the rotation matrix
-    r20 = 2*x*z-2*y*w
-    r21 = 2*y*z+2*x*w
-    r22 = 1-2*x*x-2*y*y
-     
-    # 3x3 rotation matrix
-    rot_matrix = np.array([[r00, r01, r02],
-                           [r10, r11, r12],
-                           [r20, r21, r22]])
-                            
-    return rot_matrix
+def quaternion_multiply(quaternion0: np.ndarray, quaternion1: np.ndarray) -> np.ndarray:
+    # Hamilton multiplication
+    w0, x0, y0, z0 = quaternion0
+    w1, x1, y1, z1 = quaternion1
+    return np.array([-x1 * x0 - y1 * y0 - z1 * z0 + w1 * w0,
+                     x1 * w0 + y1 * z0 - z1 * y0 + w1 * x0,
+                     -x1 * z0 + y1 * w0 + z1 * x0 + w1 * y0,
+                     x1 * y0 - y1 * x0 + z1 * w0 + w1 * z0], dtype=np.float64)
 
-def euler_rotation_matrix(roll, pitch, yaw):
-    # radiant
-    Rx = np.array([[1, 0, 0],
-                   [0, np.cos(roll), -np.sin(roll)],
-                   [0, np.sin(roll), np.cos(roll)]])
-    
-    Ry = np.array([[np.cos(pitch), 0, np.sin(pitch)],
-                   [0, 1, 0],
-                   [-np.sin(pitch), 0, np.cos(pitch)]])
-    
-    Rz = np.array([[np.cos(yaw), -np.sin(yaw), 0],
-                   [np.sin(yaw), np.cos(yaw), 0],
-                   [0, 0, 1]])
-    
-    Rxy = np.matmul(Rx, Ry)
-    R = np.matmul(Rxy, Rz)
-
-    return R
+def vector3_rotate(vector3: np.ndarray, quaternion: np.ndarray) -> np.ndarray:
+    q_vec = np.append(0.0, vector3) # 0, x, y, z
+    q_inverse = np.concatenate(([quaternion[0]], -quaternion[1:])) # w, -x, -y, -z
+    return quaternion_multiply(quaternion, quaternion_multiply(q_vec, q_inverse))[1:]
 
 
 def main():